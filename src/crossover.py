5d94e974b79e0fc2c0c73723de26336792565401
akel <ene.cristian.p7q@student.ucv.ro>
2024-12-27 16:02:27 +0200

    Enhance discrete crossover to prevent duplicates and maintain offspring size

--- src/crossover.py
+++ src/crossover.py
@@ -22,13 +22,39 @@ def discrete_crossover(selected_population):
         p1, p2 = selected_population[i], selected_population[i+1]
         rd_values = np.random.randint(0, 2, len(p1))
         c1, c2 = [], []
+        # Ensure no duplicates in c1 and c2 and maintain the size of the parents
+        c1_set, c2_set = set(c1), set(c2)
         for j in range(len(p1)):
             if rd_values[j] == 0:
-                c1.append(p1[j])
-                c2.append(p2[j])
+                if p2[j] not in c1_set and len(c1) < len(p1):
+                    c1.append(p2[j])
+                    c1_set.add(p2[j])
+                if p1[j] not in c2_set and len(c2) < len(p1):
+                    c2.append(p1[j])
+                    c2_set.add(p1[j])
             else:
-                c1.append(p2[j])
-                c2.append(p1[j])
+                if p1[j] not in c1_set and len(c1) < len(p1):
+                    c1.append(p1[j])
+                    c1_set.add(p1[j])
+                if p2[j] not in c2_set and len(c2) < len(p1):
+                    c2.append(p2[j])
+                    c2_set.add(p2[j])
+        # Fill in any missing genes to maintain the size of the parents
+        for gene in p1:
+            if len(c1) < len(p1) and gene not in c1_set:
+                c1.append(gene)
+                c1_set.add(gene)
+            if len(c2) < len(p1) and gene not in c2_set:
+                c2.append(gene)
+                c2_set.add(gene)
+
+        for gene in p2:
+            if len(c1) < len(p2) and gene not in c1_set:
+                c1.append(gene)
+                c1_set.add(gene)
+            if len(c2) < len(p2) and gene not in c2_set:
+                c2.append(gene)
+                c2_set.add(gene)
         offspring.extend([c1, c2])
     return offspring
     
